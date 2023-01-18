eb6187e3d150d4bdbe92b8548e73347d2712f101
L.F~ <110194660+luizfxz@users.noreply.github.com>
2023-01-18 09:09:16 -0300

    Add files via upload

---
+++ javascript/variaveis.js
@@ -0,0 +1,71 @@
+var pool = 2; // Número --> Variável iniciada com um valor
+
+var nome = "Fulano"; // String
+
+let time; // Utilizada quando é necessário de uma variável para determinado bloco do código
+
+const doce = "chocolate"; // Variável que não muda de valor, somente para leitura
+
+if (pool == 2 && pool == 3) {
+
+    //executa esse bloco
+
+}
+
+if (pool == 2 || pool == 3) {
+
+    //executa esse bloco
+
+}
+
+
+
+
+switch(pool) {
+
+    case 1: 
+
+        //executa esse bloco
+
+        break;
+    
+    case 2: 
+
+        // executa esse bloco
+
+        break;
+
+    default:
+
+        // executa esse bloco
+
+        break;
+
+}
+
+for( var i = 0; i <= 10; i++) {
+
+    // Irá repetir até o 10
+
+}
+
+while (pool == 2) {
+
+    // execute esse bloco
+
+
+}
+
+
+
+do {
+
+    // execute esse bloco
+} while (pool != 2) 
+
+
+var pool = ["dog", "cat", "fish"];
+
+
+
+
